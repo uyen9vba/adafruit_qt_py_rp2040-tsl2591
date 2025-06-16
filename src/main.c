8f8ee1d03bb19816646842c32c3fa451d1e4f821
uyen9vba <xcb3djok@gmail.com>
2025-06-16 20:34:52 +0300

    feat: tsl2591 sensor reading

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@ LOG_MODULE_REGISTER(main);
 #include <zephyr/drivers/led_strip.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/spi.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/usb/usb_device.h>
@@ -45,40 +46,58 @@ static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart), "Console device is not ACM CDC UART device");
 static const struct device *const uart = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 static const struct device *const i2c = DEVICE_DT_GET(DT_NODELABEL(i2c1));
+static const struct device *const tsl2591 = DEVICE_DT_GET(DT_NODELABEL(tsl2591));
 
-int main(void)
-{
-	size_t color = 0;
-	int ret = 0;
+int main(void) {
+    int ret = 0;
+    size_t color = 0;
+    struct sensor_value value_all, value_light, value_ir;
 
-	if (!device_is_ready(uart))
-		return 0;
-
-	if (usb_enable(NULL))
+    if (!device_is_ready(uart))
         return 0;
 
-	if (!device_is_ready(strip))
-		return 0;
+    if (usb_enable(NULL))
+        return 0;
 
-	while (1) {
+    if (!device_is_ready(strip))
+        return 0;
 
-        printk("Hello World! %s\n", CONFIG_BOARD_TARGET);
+    if (!device_is_ready(tsl2591))
+        return 0;
 
-        uint8_t error = 0u;
-        uint8_t dst;
-        struct i2c_msg msgs[1];
-        msgs[0].buf = &dst;
-        msgs[0].len = 1U;
-        msgs[0].flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+    printk("%s\n", CONFIG_BOARD_TARGET);
 
-        for (uint16_t x = 0; x <= 0x7f; x++) {
-            error = i2c_transfer(i2c, &msgs[0], 1, x);
-            if (error == 0)
-                printk("I2C FOUND: 0x%02x\n", x);
-            else
-                printk("i2c error %d\n", error);
-		    k_sleep(K_MSEC(100));
+    while (1) {
+
+        /* Unfortunately, datasheet does not provide a lux conversion formula for this particular
+         * device. There is still ongoing discussion about the proper formula, though this
+         * implementation uses a slightly modified version of the Adafruit library formula:
+         * https://github.com/adafruit/Adafruit_TSL2591_Library/
+         *
+         * Since the device relies on both visible and IR readings to calculate lux,
+         * read SENSOR_CHAN_ALL to get a closer approximation of lux. Reading SENSOR_CHAN_LIGHT or
+         * SENSOR_CHAN_IR individually can be more closely thought of as relative strength
+         * as opposed to true lux.
+         */
+        ret = sensor_sample_fetch(tsl2591);
+        if (ret) {
+            printk("sensor_sample_fetch error %d", ret);
+            continue;
         }
+
+        ret |= sensor_channel_get(tsl2591, SENSOR_CHAN_ALL, &value_all);
+        ret |= sensor_channel_get(tsl2591, SENSOR_CHAN_LIGHT, &value_light);
+        ret |= sensor_channel_get(tsl2591, SENSOR_CHAN_IR, &value_ir);
+        if (ret) {
+            printk("sensor_channel_get error %d", ret);
+            continue;
+        }
+
+        printk("SENSOR_CHAN_ALL     %d,%d\n", value_all.val1, value_all.val2);
+        printk("SENSOR_CHAN_LIGHT   %d,%d\n", value_light.val1, value_light.val2);
+        printk("SENSOR_CHAN_IR      %d,%d\n", value_ir.val1, value_ir.val2);
+
+        k_sleep(K_MSEC(1000));
 
         /*
 		for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
