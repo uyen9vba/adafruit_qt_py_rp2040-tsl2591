566bf8c024784934fead166b6755bd6d4c70be93
uyen9vba <xcb3djok@gmail.com>
2025-06-20 13:40:53 +0300

    feat: json output uart data

--- src/main.c
+++ src/main.c
@@ -22,6 +22,7 @@ LOG_MODULE_REGISTER(main);
 #include <zephyr/drivers/uart.h>
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/data/json.h>
 
 #define STRIP_NODE		DT_ALIAS(led_strip)
 
@@ -33,25 +34,32 @@ LOG_MODULE_REGISTER(main);
 
 #define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
 
-static const struct led_rgb colors[] = {
-	RGB(0x10, 0x00, 0x00), /* red */
-	RGB(0x00, 0x10, 0x00), /* green */
-	RGB(0x00, 0x00, 0x10), /* blue */
-};
-
-static struct led_rgb pixels[STRIP_NUM_PIXELS];
-
 static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart), "Console device is not ACM CDC UART device");
 static const struct device *const uart = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-static const struct device *const i2c = DEVICE_DT_GET(DT_NODELABEL(i2c1));
 static const struct device *const tsl2591 = DEVICE_DT_GET(DT_NODELABEL(tsl2591));
 
 int main(void) {
     int ret = 0;
-    size_t color = 0;
+    struct led_rgb color = {.r = 0x00, .g = 0x00, .b = 0x00};
     struct sensor_value value_all, value_light, value_ir;
+    int32_t lux;
+    uint16_t output;
+
+    struct json_data {
+        int32_t lux;
+        int32_t visible;
+        int32_t infrared;
+        //uint16_t output;
+    } json;
+    static const struct json_obj_descr descr[] = {
+        JSON_OBJ_DESCR_PRIM(struct json_data, lux, JSON_TOK_NUMBER),
+        JSON_OBJ_DESCR_PRIM(struct json_data, visible, JSON_TOK_NUMBER),
+        JSON_OBJ_DESCR_PRIM(struct json_data, infrared, JSON_TOK_NUMBER),
+        //JSON_OBJ_DESCR_PRIM(struct json_data, output, JSON_TOK_NUMBER),
+    };
+
 
     if (!device_is_ready(uart))
         return 0;
@@ -81,7 +89,7 @@ int main(void) {
          */
         ret = sensor_sample_fetch(tsl2591);
         if (ret) {
-            printk("sensor_sample_fetch error %d", ret);
+            printk("sensor_sample_fetch %d", ret);
             continue;
         }
 
@@ -89,26 +97,51 @@ int main(void) {
         ret |= sensor_channel_get(tsl2591, SENSOR_CHAN_LIGHT, &value_light);
         ret |= sensor_channel_get(tsl2591, SENSOR_CHAN_IR, &value_ir);
         if (ret) {
-            printk("sensor_channel_get error %d", ret);
+            printk("sensor_channel_get %d", ret);
             continue;
         }
+        
+        color.g = 0x10;
+        ret = led_strip_update_rgb(strip, &color, STRIP_NUM_PIXELS);
+        if (ret) {
+            LOG_ERR("led_strip_update_rgb %d", ret);
+        }
 
-        int32_t lux_value = value_all.val1;
+        lux = value_all.val1;
 
-        uint16_t lux_min = 20;
-        uint16_t lux_max = 6000;
+        uint16_t lux_min = 0;
+        uint16_t lux_max = 800;
         uint16_t lux_range = lux_max - lux_min;
         uint16_t output_min = 0;
         uint16_t output_max = 80;
         uint16_t output_range = output_max - output_min;
-        uint16_t output = (((lux_value - lux_min) * output_range) / lux_range) + output_min;
+        output = (((lux - lux_min) * output_range) / lux_range) + output_min;
 
+        /*
         printk("SENSOR_CHAN_ALL     %d,%d\n", value_all.val1, value_all.val2);
         printk("SENSOR_CHAN_LIGHT   %d,%d\n", value_light.val1, value_light.val2);
         printk("SENSOR_CHAN_IR      %d,%d\n", value_ir.val1, value_ir.val2);
-        printk("lux, output         %d,%d\n\n", lux_value, output);
+        printk("lux, output         %d,%d\n\n", lux, output);
+*/
+        json.lux = lux;
+        json.visible = value_light.val1;
+        json.infrared = value_ir.val1;
+        //json.output = output;
+        char buf[256] = {0};
+        ret = json_obj_encode_buf(descr, ARRAY_SIZE(descr), &json, buf, sizeof(buf));
+        if (ret != 0)
+            printk("json error %d\n", ret);
+        else
+            printk("%s\n", buf);
 
-        k_sleep(K_MSEC(1000));
+        k_sleep(K_MSEC(100));
+        color.g = 0x00;
+        ret = led_strip_update_rgb(strip, &color, STRIP_NUM_PIXELS);
+        if (ret) {
+            LOG_ERR("led_strip_update_rgb %d", ret);
+        }
+
+        k_sleep(K_MSEC(900));
 
         /*
 		for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
